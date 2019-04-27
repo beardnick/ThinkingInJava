fd2f4061ad9ecc442f9a83f8180dda421572f151
beardnick <1685437606@qq.com>
2019-04-27 21:16:55 +0800

    finish producer consumer,figure out the reason of failure

--- src/concurrent/SimpleProducerConsumer.java
+++ src/concurrent/SimpleProducerConsumer.java
@@ -6,7 +6,8 @@ public class SimpleProducerConsumer {
 
     private static final int FULL = 1000;
     private static final int EMPTY = 0;
-    private static Integer size = EMPTY;
+    private static volatile Integer size = EMPTY;
+    private static String lock = "lock";
 
     public static boolean isFull() {
         return size.equals(FULL);
@@ -24,20 +25,22 @@ public class SimpleProducerConsumer {
 
         @Override
         public void run() {
-            synchronized (size) {
+//#IMP 19-04-27 这里使用第三变量String作为锁是因为这个String一直没有被修改过
+//，如果使用的是size（Integer）每次++都会生成新的Integer实例，与锁住的size不是同一对象，就会造成java.lang.IllegalMonitorStateException
+            synchronized (lock) {
 //#IMP 19-04-27 永远都在循环中使用wait,因为某种原因，wait和能在条件不满足的情况下被唤醒
 //                wait 只能在同步块中使用
                 while (isEmpty()) {
                     try {
-                        size.wait();
+                        lock.wait();
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                         Thread.interrupted();
                     }
                 }
-                size++;
+                size--;
                 System.out.println("consumer " + Thread.currentThread().getName() + ":" + size);
-                size.notifyAll();
+                lock.notifyAll();
             }
         }
     }
@@ -47,18 +50,18 @@ public class SimpleProducerConsumer {
 
         @Override
         public void run() {
-            synchronized (size) {
+            synchronized (lock) {
                 while (isFull()) {
                     try {
-                        size.wait();
+                        lock.wait();
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                         Thread.interrupted();
                     }
                 }
-                size--;
+                size++;
                 System.out.println("producer " + Thread.currentThread().getName() + ":" + size);
-                size.notifyAll();
+                lock.notifyAll();
             }
         }
 
@@ -66,8 +69,11 @@ public class SimpleProducerConsumer {
 
     public static void main(String[] args) {
         for (int i = 0; i < 100; i++) {
-            new Thread(new WaiteConsumer()).start();
             new Thread(new WaiteProducer()).start();
         }
+        for (int i = 0; i < 10; i++) {
+            new Thread(new WaiteConsumer()).start();
+        }
+
     }
 }
